1882cf06a3400c385591b8efdde0ae02e31fccb9
Matt Burke <burkemd1+github@gmail.com>
2014-07-14 21:22:04 -0400

    Home  page

--- LandmineWeb/App_Start/RouteConfig.cs
+++ LandmineWeb/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace LandmineWeb
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Play",
+                url: "play",
+                defaults: new { controller = "Game", action = "Play" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
