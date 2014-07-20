a6443cd5e9fb16d5eb36af7f2b9810c0ae95f8a3
Matt Burke <burkemd1+github@gmail.com>
2014-07-20 12:29:52 -0400

    add rollbar js and google analytics

---
+++ LandmineWeb/Environment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace LandmineWeb
+{
+    public static class Environment
+    {
+        private static Lazy<string> environmentName;
+
+        static Environment()
+        {
+            environmentName = new Lazy<string>(() => WebConfigurationManager.AppSettings["Landmine.Environment"] ?? "production");
+        }
+
+        public static string Name
+        {
+            get { return environmentName.Value; }
+        }
+
+        public static bool IsProduction
+        {
+            get { return Name == "production"; }
+        }
+
+        public static bool IsDevelopment
+        {
+            get { return Name == "development"; }
+        }
+
+    }
+}
