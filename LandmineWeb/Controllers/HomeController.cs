16ee88f8eb94de9b93c2dc2cbd7faac463a252b7
Matt Burke <burkemd1+github@gmail.com>
2014-07-21 20:56:29 -0400

    remove debugging throw action

--- LandmineWeb/Controllers/HomeController.cs
+++ LandmineWeb/Controllers/HomeController.cs
@@ -12,10 +12,5 @@ namespace LandmineWeb.Controllers
         {
             return View();
         }
-
-        public ActionResult Throw()
-        {
-            throw new Exception("Testing Rollbar integration");
-        }
     }
 }
