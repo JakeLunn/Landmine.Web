e2a72e678f2f00a83909fb62926610f50134eeef
Matt Burke <git@mattburkedev.com>
2015-12-20 20:16:49 +0000

    move url helpers to other helpers namespace

--- LandmineWeb/HtmlHelpers/UrlHelperExtensions.cs
+++ LandmineWeb/HtmlHelpers/UrlHelperExtensions.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LandmineWeb.Helpers
+namespace LandmineWeb.HtmlHelpers
 {
     public static class UrlHelperExtensions
     {
         public static string AbsoluteAction(this UrlHelper url, string actionName, string controllerName, object routeValues)
         {
-            return url.Action(actionName, controllerName, routeValues, url.RequestContext.HttpContext.Request.Url.Scheme);
+            return url.Action(actionName, controllerName, routeValues, url.RequestContext.HttpContext?.Request?.Url?.Scheme ?? "http");
         }
 
         public static string AbsoluteContent(this UrlHelper url, string path)
@@ -23,9 +24,11 @@ namespace LandmineWeb.Helpers
             }
 
             var requestUrl = url.RequestContext.HttpContext.Request.Url;
-            var builder = new UriBuilder(requestUrl.Scheme, requestUrl.Host, requestUrl.Port);
+            var scheme = requestUrl?.Scheme ?? "http";
 
-            builder.Path = VirtualPathUtility.ToAbsolute(path);
+            Debug.Assert(requestUrl != null, "requestUrl != null");
+            var builder = new UriBuilder(scheme, requestUrl.Host, requestUrl.Port) { Path = VirtualPathUtility.ToAbsolute(path) };
+
             uri = builder.Uri;
 
             return uri.ToString();
