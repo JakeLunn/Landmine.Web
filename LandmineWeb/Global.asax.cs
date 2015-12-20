d9db7359ee64b51cc1a2db02119f806d745b9f6c
Matt Burke <git@mattburkedev.com>
2015-12-20 05:09:50 +0000

    autofac wireup

--- LandmineWeb/Global.asax.cs
+++ LandmineWeb/Global.asax.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+using Autofac;
+using Autofac.Integration.Mvc;
+
+using Landmine.Domain.Abstract;
+using Landmine.Domain.Concrete;
 
 using LandmineWeb.App_Start;
 
@@ -24,8 +31,42 @@ namespace LandmineWeb
             System.Web.Optimization.BundleTable.EnableOptimizations = false;
 #endif
 
-            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
-            GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(NinjectKernelFactory.Kernel);
+            AutofacConfig.Register();
+        }
+    }
+
+    public static class AutofacConfig
+    {
+        public static void Register()
+        {
+            var builder = new ContainerBuilder();
+
+            RegisterServices(builder);
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            //builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            //builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            //builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            //builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            //builder.RegisterFilterProvider();
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterType<ScoreRepository>().AsImplementedInterfaces();
         }
     }
 }
